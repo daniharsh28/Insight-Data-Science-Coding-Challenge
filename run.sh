924a29ff8cfd71ecd55591fc1da1c70c400ec093
Harsh Dani <daniharsh28@gmail.com>
2015-03-22 15:03:56 -0700

    Adding run.sh !

---
+++ run.sh
@@ -0,0 +1,16 @@
+#Compiling java programs and then running it
+
+javac src/FindMedian.java
+javac src/WordCount.java
+
+#Change directory to src
+cd src
+
+#Run java programs
+
+java WordCount
+java FindMedian
+
+echo "---------------------------"
+echo "Script Complete! Thank you!"
+#Script Complete
